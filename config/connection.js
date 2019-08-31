13df4767acddbb2aa98b22be245fa9234faf20d9
Casey Carroll <caseydean8@gmail.com>
2019-08-31 12:55:38 -0400

    converted connection file to sequelize object to be exported.

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,43 @@
-var mysql = require("mysql");
+const Sequelize = require('sequelize')
 
-let connection;
-
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
-  connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "fatbeats",
-  database: "burgers_db"
-});
-}
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+// Create mySQL connection using Sequelize.
+const sequelize = new Sequelize('burgers_db', 'root', 'fatbeats', {
+    host: 'localhost',
+    port: 3306,
+    dialect: mysql,
+    // (property)  sequelize.Options.pool?: sequelize.PoolOptions
+    // connection pool options
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }
 });
 
-module.exports = connection;
+module.exports = sequelize;
+
+// var mysql = require("mysql");
+
+// let connection;
+
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL)
+// } else {
+//   connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "fatbeats",
+//   database: "burgers_db"
+// });
+// }
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// module.exports = connection;
