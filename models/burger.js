d9658e16dc92ed9e912f7ba4bc09967cae393521
Casey Carroll <caseydean8@gmail.com>
2019-09-02 20:09:18 -0400

    Created burger model.

--- models/burger.js
+++ models/burger.js
@@ -1,41 +1,57 @@
-// import orm (will '../config/orm' work)
-// const orm = require('../config/orm.js')
+// // import orm (will '../config/orm' work)
+// // const orm = require('../config/orm.js')
 
-// remake with sequelize
+// // remake with sequelize
 
-// Sequelize references the standard library
-const Sequelize = require('sequelize')
+// // Sequelize references the standard library
+// const Sequelize = require('sequelize')
 
-// sequelize references the connection to the database
-const sequelize = ('../config/connection.js')
+// // sequelize references the connection to the database
+// const sequelize = ('../config/connection.js')
 
-const burger = {
-    selectAll: function(a) {
-        console.log('burger model contact')
-        orm.selectAll('burgers', (res) => {
-            a(res)
-        })
-    },
+// const burger = {
+//     selectAll: function(a) {
+//         console.log('burger model contact')
+//         orm.selectAll('burgers', (res) => {
+//             a(res)
+//         })
+//     },
 
-    insertOne: function(columns, values, a) {
-        console.log('burger model contact')
-        orm.insertOne('burgers', columns, values, function(res) {
-            a(res)
-        })
-    },
-    updateOne: function(dvrdval, status, a) {
-        console.log("burger model update contact")
-        orm.updateOne('burgers', dvrdval, status, function(res) {
-            a(res)
-        })
-    },
-    delete: function(status, a) {
-        console.log("burger model delete contact")
-        orm.delete('burgers', status, function(res) {
-            a(res)
-        })
-    }
-}
+//     insertOne: function(columns, values, a) {
+//         console.log('burger model contact')
+//         orm.insertOne('burgers', columns, values, function(res) {
+//             a(res)
+//         })
+//     },
+//     updateOne: function(dvrdval, status, a) {
+//         console.log("burger model update contact")
+//         orm.updateOne('burgers', dvrdval, status, function(res) {
+//             a(res)
+//         })
+//     },
+//     delete: function(status, a) {
+//         console.log("burger model delete contact")
+//         orm.delete('burgers', status, function(res) {
+//             a(res)
+//         })
+//     }
+// }
 
 // export to controller
-module.exports = burger
+//module.exports = burger
+
+module.exports = (sequelize, data) => {
+    const burgers = sequelize.define('burgers', {
+        burger_name: {
+            type: data.STRING,
+            allowNull: false,
+            validate: { len: [1, 140] }
+        },
+        devoured: {
+            type: data.BOOLEAN,
+            defaultValue: false
+        }
+    });
+    console.log(`model burger.js hit`);
+    return burgers;
+};
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 8080
 
 const app = express()
 
+const db = require('./models')
+
+
 // serve static content from public directory
 app.use(express.static('public'))
 
@@ -16,14 +19,18 @@ const ehb = require('express-handlebars')
 
 app.engine('handlebars', ehb({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-//             check later to see if burger_controller.js is necessary
+    //             check later to see if burger_controller.js is necessary
 const routes = require('./controllers/burgers_controller.js')
 
 app.use(routes)
 
-app.listen(PORT, function() {
-    console.log(`server listening on http:/localhost: ${PORT}`)
-})
+db.sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => console.log(`server.js started, App listening on PORT ${PORT}`));
+}).catch((err) => console.log(err))
+
+// app.listen(PORT, function() {
+//     console.log(`server listening on http:/localhost: ${PORT}`)
+// })
 
 // const orm = require('./config/orm')
 
