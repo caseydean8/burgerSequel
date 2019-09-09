3a3c10f4182222ed707a8a5dc6a4794af064e36c
Casey Carroll <caseydean8@gmail.com>
2019-09-09 10:07:32 -0400

    Modified connection and config .json for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,23 @@
 const Sequelize = require('sequelize')
 
-// Create mySQL connection using Sequelize.
-const sequelize = new Sequelize('burgers_db', 'root', 'fatbeats', {
-    host: 'localhost',
-    port: 3306,
-    dialect: mysql,
-    // (property)  sequelize.Options.pool?: sequelize.PoolOptions
-    // connection pool options
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    }
-});
+let connection;
 
-module.exports = sequelize;
+// Create mySQL connection using Sequelize.
+if (process.env.JAWSDB_URL) {
+    connection = new Sequelize(process.env.JAWSDB_URL)
+} else {
+    connection = new Sequelize('burgers_db', 'root', 'fatbeats', {
+        host: 'localhost',
+        port: 3306,
+        dialect: mysql,
+        // (property)  sequelize.Options.pool?: sequelize.PoolOptions
+        // connection pool options
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        }
+    });
+}
+
+module.exports = connection;
